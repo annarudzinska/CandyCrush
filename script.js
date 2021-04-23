9a7b2f62cd883c6c699852fdeef26999755d3673
Anna Rudzińska <a.rudzinska97@gmail.com>
2021-04-23 15:32:41 +0200

    Adding matches for 5 candies Adding "moves" count Changing the score board into a table

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
     const grid = document.querySelector('.grid');
     const scoreDisplay = document.getElementById('score');
+    const movesDisplay = document.getElementById('moves');
     const width = 8;
     const squares = [];
     let score = 0;
+    let moves = 0;
 
     const candyColors = [
         'url(Candies/green.png)',
@@ -66,6 +68,8 @@ document.addEventListener('DOMContentLoaded', () => {
         sqaureIdBeingReplaced = parseInt(this.id);
         this.style.backgroundImage = colorBeingDragged; // exchanging the colors
         squares[squareIdBeingDragged].style.backgroundImage = colorBeingReplaced; //when we drop a square onto another, the latter gets the color of the one being dragged
+        moves += 1;
+        movesDisplay.innerHTML = moves; 
         check();
     }
 
@@ -88,7 +92,9 @@ document.addEventListener('DOMContentLoaded', () => {
         } else if (sqaureIdBeingReplaced && !validMove) {
             squares[sqaureIdBeingReplaced].style.backgroundImage = colorBeingReplaced; // if the move is not valid we give the square its original color
             squares[squareIdBeingDragged].style.backgroundImage = colorBeingDragged; //as above
-        } else squares[squareIdBeingDragged].style.backgroundImage = colorBeingDragged;
+        } else {
+            squares[squareIdBeingDragged].style.backgroundImage = colorBeingDragged;
+        }    
 
     }
 
@@ -116,6 +122,47 @@ function moveDown() {
 
 
 //Checking for matches
+
+//checking for 5 the same colors in a row
+
+function checkRowForFive() {
+    for (i = 0; i <= 59; i++) { //we cannot go over 63
+        let rowOfFive = [i, i+1, i+2, i+3, i+4];
+        let decidedColor = squares[i].style.backgroundImage;
+        const isBlank = squares[i].style.backgroundImage === ''; //if the square doesn't have a color it is blank
+
+        const notValid = [4, 5, 6, 7, 12, 13, 14, 15, 20, 21, 22, 23, 28, 29, 30, 31, 36, 37, 38, 39, 44, 45, 46, 47, 52, 53, 54, 55]; // these are two last squares in the row, if it would start there, the 3rd one goes to the other one
+        if (notValid.includes(i)) continue; // 'continue keyword is usually used to break iteration and pick it up in the case a statement is not valid
+
+        if (rowOfFive.every(index => squares[index].style.backgroundImage === decidedColor && !isBlank)) {
+            score += 5;
+            scoreDisplay.innerHTML = score;
+            rowOfFive.forEach(index => {
+                squares[index].style.backgroundImage = ''; //if we have a match, all three squares become blank
+            })
+        }
+    }
+}
+//checkRowForThree();
+
+
+
+//checking for 5 the same colors in a column
+function checkColumnForFive() {
+    for (i = 0; i <= 38; i++) { //we cannot go over 63
+        let columnOfFive = [i, i + width, i + width * 2, i + width * 3, i + width * 4];
+        let decidedColor = squares[i].style.backgroundImage;
+        const isBlank = squares[i].style.backgroundImage === ''; //if the square doesn't have a color it is blank
+
+        if (columnOfFive.every(index => squares[index].style.backgroundImage === decidedColor && !isBlank)) {
+            score += 5;
+            scoreDisplay.innerHTML = score;
+            columnOfFive.forEach(index => {
+                squares[index].style.backgroundImage = ''; //if we have a match, all three squares become blank
+            })
+        }
+    }
+}
 
 //checking for 4 the same colors in a row
 
@@ -193,6 +240,7 @@ function checkColumnForThree() {
         if (columnOfThree.every(index => squares[index].style.backgroundImage === decidedColor && !isBlank)) {
             score += 3;
             scoreDisplay.innerHTML = score;
+            console.log(moves);
             columnOfThree.forEach(index => {
                 squares[index].style.backgroundImage = ''; //if we have a match, all three squares become blank
             })
@@ -201,7 +249,9 @@ function checkColumnForThree() {
 }
 
 let check = function()  { // FOR LATER: ADD A BUTTON TO ACTIVATE IT (CHECK IN TETRIS) 
-    checkRowForFour() //checking for four first, as they are more important
+    checkColumnForFive()
+    checkRowForFive()
+    checkRowForFour() //checking for bigger first, as they are more important
     checkColumnForFour()
     checkRowForThree()
     checkColumnForThree()
